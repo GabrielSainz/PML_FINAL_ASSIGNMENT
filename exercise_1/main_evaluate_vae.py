b5c779c774c6cbd30d9ecc05c4c7697e9d9f1379
Gabriel Sainz <ga.sainz08@hotmail.com>
2026-01-09 19:33:42 +0100

    evaluation module error

--- exercise_1/main_evaluate_vae.py
+++ exercise_1/main_evaluate_vae.py
@@ -3,10 +3,11 @@ import torch
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
+root_path="/content/drive/MyDrive/University_of_Copenhagen/block6/PML/final_assignment/exercise_1" 
 rows = evaluate_all_runs(
-    run_root="./runs_vae",
-    out_dir="./runs_vae/summary",
-    data_dir="./mnist_data",
+    run_root=f"{root_path}/runs_vae",
+    out_dir=f"{root_path}/runs_vae/summary",
+    data_dir=f"{root_path}/mnist_data",
     device=device,
     n_gen=5000,            # increase to 10000 if you want
     validity_thresh=0.9,
